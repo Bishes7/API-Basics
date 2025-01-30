a5d1fbe7f21219ab38e4d8693b2389ea95c08aaf
Bishes7 <specialbishes9@gmail.com>
2025-01-30 16:40:22 +1100

    get method tested from the API

--- server.js
+++ server.js
@@ -6,3 +6,37 @@ const PORT = 8000;
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`http://localhost:${PORT}`);
 });
+
+const DB = [
+  {
+    id: 1,
+    fName: "Bishes ",
+    lName: "Adhikari",
+  },
+  {
+    id: 2,
+    fName: "Umesh",
+    lName: "Dhakal",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "todo,",
+    DB,
+  });
+  console.log(req.query);
+});
+
+app.post("/", (req, res) => {
+  res.json({
+    message: "post method",
+    user: DB,
+  });
+});
+
+app.put("/", (req, res) => {
+  res.json({
+    message: "put method",
+  });
+});
